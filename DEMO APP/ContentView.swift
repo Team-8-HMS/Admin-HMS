ae8592e92f11429def4a0ddae08d5b6517edb386
Aman00065 <prakash07aman@gmail.com>
2024-07-03 11:20:50 +0530

    Initial Commit

---
+++ DEMO APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DEMO APP
+//
+//  Created by Aman Prakash on 03/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DEMO APP/DEMO_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO_APPApp.swift
+//  DEMO APP
+//
+//  Created by Aman Prakash on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DEMO_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
